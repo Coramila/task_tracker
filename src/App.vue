89e451c518e263cc85d8144f9fecf86bad1a3718
Camila Carvalho <carvalho.camiilaa@gmail.com>
2022-12-03 20:46:12 -0300

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+ 
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'App',
+ 
+});
+</script>
+
+<style>
+
+</style>
