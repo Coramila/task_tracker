a5aef4603ef5555909d01df432bc12a7514f6b8f
Camila Carvalho <carvalho.camiilaa@gmail.com>
2022-12-04 11:18:20 -0300

    laterar bar

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,37 @@
+<template>
+<header>
+    <h1>
+        <img src="../assets/logo_.png" alt="">
+    </h1>
+</header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    name: 'BarraLateral'
+})
+</script>
+
+<style scoped>
+
+header{
+    background-color: rgb(0 74 173);
+    width: 100%;
+    height: 100vh;
+}
+
+header img {
+    border-radius: 30px;
+    margin: 1rem;
+}
+
+@media only screen and (max-width: 768px){
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+
+</style>
