d20cf16374c963b1b8b8d3c57bffe33961bce182
Camila Carvalho <carvalho.camiilaa@gmail.com>
2022-12-05 20:29:35 -0300

    Editing form column

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
 import { defineComponent } from 'vue';
 
 import BarraLateral from './components/BarraLateral.vue'
-import Formulario from './components/BarraLateral.vue'
+import Formulario from './components/Formulario.vue'
 export default defineComponent({
   name: 'App',
   components:{
